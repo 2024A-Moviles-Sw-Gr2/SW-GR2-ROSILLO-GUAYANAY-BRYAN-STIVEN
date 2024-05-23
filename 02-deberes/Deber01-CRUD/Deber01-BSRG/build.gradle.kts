cb4f827c3af616150152fe54bb29ba4d4cfb739c
BryanRosillo <bryan.rosillo@epn.edu,ec>
2024-05-23 07:22:20 -0500

    fix: project kotlin

--- 02-deberes/Deber01-CRUD/Deber01-BSRG/build.gradle.kts
+++ 02-deberes/Deber01-CRUD/Deber01-BSRG/build.gradle.kts
@@ -1,5 +1,7 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "1.6.21"
 }
 
 group = "org.example"
@@ -15,4 +17,8 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
