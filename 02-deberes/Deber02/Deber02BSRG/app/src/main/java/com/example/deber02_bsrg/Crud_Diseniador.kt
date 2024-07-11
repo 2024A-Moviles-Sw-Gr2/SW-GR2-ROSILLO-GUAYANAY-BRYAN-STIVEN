547b1706fa477c12a5bd68f901323603bf792f5f
BryanRosillo <bryan.rosillo@epn.edu,ec>
2024-07-10 20:57:06 -0500

    feat: add new mobile project

---
+++ 02-deberes/Deber02/Deber02BSRG/app/src/main/java/com/example/deber02_bsrg/Crud_Diseniador.kt
@@ -0,0 +1,11 @@
+package com.example.deber02_bsrg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Crud_Diseniador : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crud_diseniador)
+    }
+}
